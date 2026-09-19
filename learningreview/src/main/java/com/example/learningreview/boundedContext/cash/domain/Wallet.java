c3acbea41570d1e8b478004eeaa3f615baf61a5e
khb <khb3025@gmail.com>
2026-09-20 00:19:31 +0900

    0024 wallet 테이블 명 수정

--- learningreview/src/main/java/com/example/learningreview/boundedContext/cash/domain/Wallet.java
+++ learningreview/src/main/java/com/example/learningreview/boundedContext/cash/domain/Wallet.java
@@ -4,12 +4,14 @@ import com.example.learningreview.global.jpa.entity.BaseManualIdAndTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Table(name = "CASH_WALLET")
 public class Wallet extends BaseManualIdAndTime {
 
     @OneToOne(fetch = FetchType.LAZY)
